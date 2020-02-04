913120abbeb0f82e06cf44aafabd481aef47ebad
jorge <jorgeluis.imperial@gmail.com>
2020-02-04 09:08:57 -0800

    Added arguments to set up connection to hosts

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  url: 'mongodb://localhost:31000/test?replicaSet=rs',
+  url: 'mongodb://user:password@localhost:31000/test?replicaSet=rs&authSource=admin',
   options: { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 10 }
 };
